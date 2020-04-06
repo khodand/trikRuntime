37d3edf6429fc79124d29fc0118d9025d335680b
Andrei <andrei.khodko@gmail.com>
2020-04-07 02:30:23 +0300

    Squash testing

--- trikControl/src/utilities.cpp
+++ trikControl/src/utilities.cpp
@@ -39,8 +39,10 @@ QImage  Utilities::imageFromBytes(const QVector<int32_t> &array, int width, int 
 
 	if (!format.compare("rgb32", Qt::CaseInsensitive)) {
 		if (width * height <= array.size()) {
-			return QImage(static_cast<const uchar *>(static_cast<const void *>(array.data()))
-				      , width, height, QImage::Format_RGB32);
+			auto code = static_cast<const uchar *>(static_cast<const void *>(array.data()));
+			formattedData = std::copy(code, code + width * height , formattedData);
+//			return QImage(static_cast<const uchar *>(static_cast<const void *>(array.data()))
+//				      , width, height, QImage::Format_RGB32);
 		}
 	} else if (!format.compare("rgb888", Qt::CaseInsensitive)) {
 		fmt = QImage::Format_RGB888;
