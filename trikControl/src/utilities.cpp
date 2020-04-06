e4e0f6ad8e1e4b774f8d993f4f4c2b2ff53a28a0
Andrei <andrei.khodko@gmail.com>
2020-04-06 11:50:10 +0300

    safdasf

--- trikControl/src/utilities.cpp
+++ trikControl/src/utilities.cpp
@@ -40,13 +40,16 @@ QImage  Utilities::imageFromBytes(const QVector<int32_t> &array, int width, int 
 
 	if (!format.compare("rgb32", Qt::CaseInsensitive)) {
 		fmt = QImage::Format_RGB32;
+		auto src = new int32_t[array.size()];
+		src = std::copy(array.begin(), array.end(), src);
+		formattedData = static_cast< uchar *>(static_cast< void *>(src));
 		//formattedData = new uchar[width * height];
 		//formattedData = static_cast<const uchar *>(static_cast<const void *>(array.data()));
 		//formattedData = std::copy();
 		//formattedData = const_cast<uchar *>(rawData);
 
 		//formattedData = code;
-		copyAligned(width);
+		//copyAligned(width);
 		//formattedData = static_cast<const uchar *>(static_cast<const void *>(array.data()));
 	} else if (!format.compare("rgb888", Qt::CaseInsensitive)) {
 		fmt = QImage::Format_RGB888;
