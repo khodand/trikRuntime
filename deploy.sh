aec03e25aa961e76fde3c662aee1a44babf2b58c
Andrei <andrei.khodko@gmail.com>
2020-04-15 15:09:52 +0300

    Some fix

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ else
 	echo "Usage: deploy.sh <robot ip>"
 	exit 1
 fi
-
+#asdf
 ssh root@"$ip" " sh -c 'chmod -x /etc/trik/trikGui.sh && killall trikGui'"
 rsync -avz -e ssh . root@$ip:/home/root/trik
 ssh root@"$ip" "sh -c 'chmod +x /etc/trik/trikGui.sh && kill -hup 1'"
